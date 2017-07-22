99127297170006f61631c45896555bda22b8e89c
leti <l.garcia.martin.contractor@bbva.com>
2017-07-22 16:48:37 +0200

    Adding Hello world in flask and connection to rethinkdb

--- modsrv/modsrv.py
+++ modsrv/modsrv.py
@@ -1,6 +1,28 @@
-import time
+import rethinkdb as r
+from flask import Flask
+from environconfig import EnvironConfig
+from environconfig import StringVar, IntVar
+
+class DBCfg(EnvironConfig):
+    """Database configuration from the environment."""
+    HOSTNAME = StringVar(default='rethinkdb')
+    PORT = IntVar(default=28015)
+
+app = Flask(__name__)
 
 
-while True:
-    print("Test lae", flush=True)
-    time.sleep(1)
+@app.route('/')
+def index():
+    return '<h1>Hello World!</h1>'
+
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, %s!</h1>' % name
+
+
+if __name__ == '__main__':
+    r.connect(DBCfg.HOSTNAME, DBCfg.PORT).repl()
+    r.db('test').table_create('tv_movies').run()
+    r.table('tv_shows').insert({'name': 'Star Trek TNG'}).run()
+    app.run(debug=True, port=8888)
